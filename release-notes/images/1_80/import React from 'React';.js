4b21dd5f7140baa1d3566592f6d191ad7f8e5106
idavel <166016390+idavel@users.noreply.github.com>
2024-05-04 13:56:29 -0600

    release-notes/images/1_80/.card {.css

---
+++ release-notes/images/1_80/import React from 'React';.js
@@ -0,0 +1,12 @@
+import React from 'React';
+import card from './card';
+
+function App (){
+    return (
+        <div className='App'>
+        <card Title="carta"
+        description="descripcion"/>
+        </div>
+    );
+}
+
